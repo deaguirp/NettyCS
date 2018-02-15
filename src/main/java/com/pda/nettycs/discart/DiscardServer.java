edaaf8bd5f51f593dff695229e727c8595ddfd42
Pablo de Aguirre <pablo.deaguirre.contractor@bbva.com>
2018-02-15 19:58:17 -0300

    Init

---
+++ src/main/java/com/pda/nettycs/discart/DiscardServer.java
@@ -0,0 +1,18 @@
+package com.pda.nettycs.discart;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Hello world!
+ *
+ */
+@Log4j2
+public class DiscardServer 
+{
+	
+    public static void main( String[] args )
+    {
+        log.info( "Hello World!" );
+        
+    }
+}
